56c973fef14072b01d6813c2db71541d7c159d63
Ilya Petrov <ilya.petrov.m@gmail.com>
2019-10-24 13:17:21 +0300

    Карта масштабируется на всех точках
    !Исправлены ошибки
    +Масштабирование в начале
    + Защита от пустых координат

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 var initPoint = [50.858445, 38.394921];
-var mymap = L.map('mapid').setView(initPoint, 13);
+var initDone= false;
+var mymap = L.map('mapid');
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiaWx5YXBldHJvdiIsImEiOiJjazE0MHo2ZnUwZGhsM2JtOTR4NGYweXN0In0.2SddUDwyUnihjtu6fkzR4Q', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 20,
+	id: 'mapbox.streets',
+	accessToken: 'pk.eyJ1IjoiaWx5YXBldHJvdiIsImEiOiJjazE0MHo2ZnUwZGhsM2JtOTR4NGYweXN0In0.2SddUDwyUnihjtu6fkzR4Q'
+}).addTo(mymap);
+
 function getUrlParams(search) {
     let hashes = search.slice(search.indexOf('?') + 1).split('&')
     let params = {}
@@ -14,15 +23,6 @@ var googleSheetKey = getUrlParams(document.location.search)['googleSheetKey'];
 if(googleSheetKey === undefined){
 	alert("Невозможно получить доступ к google-таблице!");
 }
-console.log(googleSheetKey);
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiaWx5YXBldHJvdiIsImEiOiJjazE0MHo2ZnUwZGhsM2JtOTR4NGYweXN0In0.2SddUDwyUnihjtu6fkzR4Q', {
-	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-	maxZoom: 5,
-	id: 'mapbox.streets',
-	accessToken: 'pk.eyJ1IjoiaWx5YXBldHJvdiIsImEiOiJjazE0MHo2ZnUwZGhsM2JtOTR4NGYweXN0In0.2SddUDwyUnihjtu6fkzR4Q'
-}).addTo(mymap);
-
 
 function AudioPoint(lat,lng, title){
     this.coords = L.latLng(lat,lng);
@@ -48,16 +48,46 @@ AudioPoint.prototype.play = function(){
     }
 }
 
+function isNum(x){
+    return !isNaN(parseInt(x));
+}
+
 var points = [];
+function getMaxLatLng(points){
+    let maxLat = points[1][0];
+    let maxLng = points[1][1];
+    for(var i=1; i<points.length; i++){
+        if(points[i][0]>maxLat && isNum(points[i][0])) maxLat = points[i][0];
+        if(points[i][1]>maxLng && isNum(points[i][1])) maxLng = points[i][1];
+    }
+    return [maxLat,maxLng];
+}
+function getMinLatLng(points){
+    let minLat = points[1][0];
+    let minLng = points[1][1];
+    for(var i=1; i<points.length; i++){
+        if(points[i][0]<minLat && isNum(points[i][0])) minLat = points[i][0];
+        if(points[i][1]<minLng && isNum(points[i][1])) minLng = points[i][1];
+    }
+    return [minLat,minLng];
+}
 function drawPoints(data){
-	console.log(data);
+    console.log(data);
 	for(var i=1; i<data.length; i++){
+	    if(data[i][0] == '' || data[i][1] == '') continue;
 		points.push(new AudioPoint(data[i][0], data[i][1], data[i][2]));
 		points[i-1].addAudio(data[i][3]);
 	}
+	
+	mymap.fitBounds([
+        getMinLatLng(data),
+        getMaxLatLng(data)
+    ]);
+	
 	for(var i=0; i<points.length; i++){
 		L.marker(points[i].coords).addTo(mymap);
 	}
+	initDone = true;
 }
 function loadData() {
         var url = "https://sheets.googleapis.com/v4/spreadsheets/" + googleSheetKey + "/values/A:D?key=AIzaSyA8o23YY060J9v-1C9cui_YkX_b9C9k2wg";
@@ -66,7 +96,11 @@ function loadData() {
           if (xmlhttp.readyState == 4) {
             dataTable = JSON.parse(xmlhttp.responseText);
 			if(dataTable.majorDimension === "ROWS"){
-				drawPoints(dataTable.values);
+			    if(dataTable.values.length > 1){ 
+				    drawPoints(dataTable.values);
+			    }else{
+			        alert('В таблице слишком мало данных. Добавьте хотя бы одну строчку с координатами.');
+			    }
 			}
           }
         };
@@ -88,11 +122,11 @@ function getNearPoint(p, pts){
 var near = new AudioPoint(0,0);
 
 function onMoveEnd(e){
+    if(!initDone) return;
     var center = mymap.getCenter();
     near.stop();
     near = getNearPoint(center,points);
-    near.play();   
+    near.play(); 
 }
-
-mymap.on('moveend', onMoveEnd);
 loadData();
+mymap.on('moveend', onMoveEnd);
